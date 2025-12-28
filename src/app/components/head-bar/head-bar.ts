0eb0cda1e9747306da2caca2c3df1e1aad810399
CarterHorton <137738454+CarterHorton@users.noreply.github.com>
2025-12-27 22:20:04 -0600

    Added Desktop Viewport and moved to component

---
+++ src/app/components/head-bar/head-bar.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-head-bar',
+  imports: [],
+  templateUrl: './head-bar.html',
+  styleUrl: './head-bar.scss',
+})
+export class HeadBar {
+
+}
