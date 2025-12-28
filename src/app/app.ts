f21b7c737e6fae66f451528801fdf5d673d0f662
CarterHorton <137738454+CarterHorton@users.noreply.github.com>
2025-12-28 17:13:51 -0600

    Created primary and secondary button components

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeadBar } from './components/head-bar/head-bar';
+import { PrimaryButton } from './components/primary-button/primary-button';
+import { SecondaryButton } from './components/secondary-button/secondary-button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeadBar],
+  imports: [RouterOutlet, HeadBar, PrimaryButton,SecondaryButton],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
