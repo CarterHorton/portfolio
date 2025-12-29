a1401e6fe4bdf0d127f10a574b709193f9962b82
CarterHorton <137738454+CarterHorton@users.noreply.github.com>
2025-12-28 20:50:45 -0600

    swapped a tags with custom links for head-bar

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeadBar } from './components/head-bar/head-bar';
-import { PrimaryButton } from './components/primary-button/primary-button';
-import { SecondaryButton } from './components/secondary-button/secondary-button';
-import { PrimaryHyperlink } from './components/primary-hyperlink/primary-hyperlink';
-import { SecondaryHyperlink } from './components/secondary-hyperlink/secondary-hyperlink';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeadBar, SecondaryHyperlink],
+  imports: [RouterOutlet, HeadBar],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
--- src/app/components/head-bar/head-bar.ts
+++ src/app/components/head-bar/head-bar.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { PrimaryHyperlink } from "../primary-hyperlink/primary-hyperlink";
+import { SecondaryHyperlink } from '../secondary-hyperlink/secondary-hyperlink';
 
 @Component({
   selector: 'app-head-bar',
-  imports: [],
+  imports: [PrimaryHyperlink, SecondaryHyperlink],
   templateUrl: './head-bar.html',
   styleUrl: './head-bar.scss',
 })
