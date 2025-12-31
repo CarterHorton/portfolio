c72a48ab976e98abe6f1f97dee737407901ae90e
CarterHorton <137738454+CarterHorton@users.noreply.github.com>
2025-12-30 19:06:09 -0600

    Added App-routes, admin and Home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomePage } from './pages/home-page/home-page';
+import { LoginForm } from './components/login-form/login-form';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: HomePage,
+    },
+    {
+        path: 'super-cool-admin',
+        component: LoginForm
+    }
+];
---
+++ src/app/pages/home-page/home-page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { ProjectMini } from '../../components/project-mini/project-mini';
+
+@Component({
+  selector: 'app-home-page',
+  imports: [ProjectMini],
+  templateUrl: './home-page.html',
+  styleUrl: './home-page.scss',
+})
+export class HomePage {
+
+}
