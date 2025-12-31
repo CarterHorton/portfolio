56d1cc2e7b9cf54126eaaa69d1fdb1361285527f
CarterHorton <137738454+CarterHorton@users.noreply.github.com>
2025-12-30 18:47:40 -0600

    Added Footer and some small touches

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { HeadBar } from './components/head-bar/head-bar';
 import { LoginForm } from "./components/login-form/login-form";
 import { ProjectMini } from "./components/project-mini/project-mini";
+import { Footer } from "./components/footer/footer";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeadBar, ProjectMini],
+  imports: [RouterOutlet, HeadBar, ProjectMini, Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
